d4d12919dee38ed45cf5b6f5ccdf49d7f751e5ab
Matt Nicholson <matthew.n.nicholson1@gmail.com>
2017-04-29 19:40:59 -0500

    ran segmentation and got features

--- segmentation.py
+++ segmentation.py
@@ -21,12 +21,18 @@ start_time=whole_thing["time"].iloc[0]
 second_time=whole_thing["time"].iloc[1]
 end_time=whole_thing["time"].iloc[-1]
 
-output=pd.DataFrame()
-output.columns=["axmax","axmin","axvar","aymax","aymin","azvar" "azmax","azmin","azvar"]
+output=pd.DataFrame(np.zeros((25,9)),columns=["axmax","axmin","axvar","aymax","aymin","azvar", "azmax","azmin","azvar"])
+
+
+
 for i in range(25):
     temp=whole_thing[["ax","ay","az"]][i*750:750*i+500]
+    print(i)
     for dim in ["ax","ay","az"]:
         output.loc[i][dim+"max"]=np.max(temp[dim])
         output.loc[i][dim+"min"]=np.min(temp[dim])
-        output.loc[i][dim+"avg"]=np.average(temp[dim])
+        
+        output.loc[i][dim+"var"]=np.var((temp[dim].astype("float")))
 #    plt.plot(whole_thing["ax"][i*750:750*i+500])
+
+output.to_csv("feature.csv")
